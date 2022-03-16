ce43eb9c2692e340c861d8152b8a7f3495e7c7dd
bananaboy319 <matteob319@gmail.com>
2022-03-16 11:19:21 +0100

    sexet splits compiles!

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,23 @@ fn main() {
 
 }
 //splits bitvec into 6bit then shoves them into u8
-fn sextet_split(bits: BitVec) -> Vec<u8>{
+fn sextet_split(mut bits: BitVec) -> Vec<u8>{
 	//check how much padding is needed, and add it
-	let padding = bits.len() % 6;
-	for x in padding {
+	let padding: u8 = (bits.len() % 6).try_into().unwrap();
+	let mut x = 0;
+	while x <= padding {
 		bits.push(false);
+		x += 1;
 	}
 	//split, then shove into u8
-	let bits_slice ::Vec<u8>;
-	for x in bits.len()/6 {
-		let u = BitSlice::<_, Lsb0>::from_slice(&bits[x..(x+6)]);
-		bits_slice.push(u.load<u8>());
+	let mut bits_slice: Vec<u8> = Vec::new();
+	let sextets: u8 = (bits.len() / 6).try_into().unwrap();
+	x = 0;
+	while x <= sextets {
+		//let u = BitSlice::<_, Lsb0>::from_slice(&bits[x..(x+6)]);
+		let u = &bits[x as usize..(x as usize + 5)];
+		bits_slice.push(u.load::<u8>());
+		x += 1;
 	}
 	return bits_slice;
 }
