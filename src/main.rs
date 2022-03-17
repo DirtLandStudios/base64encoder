9cd9296a32a324710e89b0dd83264b9fec16c80c
bananaboy319 <matteob319@gmail.com>
2022-03-17 12:45:52 +0100

    added CLI and bugfixes

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,27 @@
-use bitvec::prelude::*;
+use bitvec::prelude::*; //import bitvec lib
+
 fn main() {
+	let args: Vec<String> = std::env::args().collect();
+	match &*args[0].to_owned() {
+		"-p" => {
+			let file: Vec<u8> = std::fs::read(&args[1]).unwrap();
+			let bits: BitVec<u8> = BitVec::<u8, Lsb0>::from_slice(&file[..]); //incase we need to add spliting due to too big variables
+			let (charvalues, padding) = sextet_split(bits);
+		},
+		"-h" => {
+			println!("-p (path to file) \n or string to encode");
+		},
+		_ => {
+			panic!(); //until I come up with something better
+		}
+	}
 
 }
 //splits bitvec into 6bit then shoves them into u8
-fn sextet_split(mut bits: BitVec) -> (Vec<u8>, u8){
+fn sextet_split(mut bits: BitVec<u8>) -> (Vec<u8>, u8) {
+	//because 8 is not a multiple of 6, some multiples of 8 (number of bits) are not multiples of 6, 
+	//so we add 0s at the end to make it divisible by both
+
 	//check how much padding is needed, and add it
 	let padding: u8 = (bits.len() % 6).try_into().unwrap();
 	let mut x = 0;
@@ -11,9 +29,12 @@ fn sextet_split(mut bits: BitVec) -> (Vec<u8>, u8){
 		bits.push(false);
 		x += 1;
 	}
+
 	//split, then shove into u8
 	let mut bits_slice: Vec<u8> = Vec::new();
+	//amount of sextets we have
 	let sextets: u8 = (bits.len() / 6).try_into().unwrap();
+	//slice bits into sextets, change to u8 (char values) and shove char values into a vec
 	x = 0;
 	while x <= sextets {
 		let u = &bits[x as usize..(x as usize + 5)];
@@ -22,3 +43,8 @@ fn sextet_split(mut bits: BitVec) -> (Vec<u8>, u8){
 	}
 	return (bits_slice, padding);
 }
+/* //not yet implemented
+fn value_to_chars(charvalues: Vec<u8>) -> String {
+
+}
+*/
