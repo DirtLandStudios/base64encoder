a98c85c143ef5338b9f0d155046de91a2896e2b7
bananaboy139 <matteob319@gmail.com>
2022-04-10 20:19:27 +0200

    works correctly

--- src/encoder.rs
+++ src/encoder.rs
@@ -1,48 +1,59 @@
-use bitvec::prelude::*; //import bitvec lib
-//use serde::{Serialize, Deserialize};
+use std::io::{stdin, Read};
+use bitvec::prelude::*;
 
-pub fn encode(input: Vec<u8>) {
-	let bits: BitVec<u8> = BitVec::<u8, Lsb0>::from_slice(&input);
-	let (charvalues, paddingvalues) = sextet_split(bits);
-	let padding = "=".repeat(paddingvalues);
-	let encoded = value_to_chars(charvalues) + &padding;
-	println!("{}", encoded);
-}
-//splits bitvec into 6bit then shoves them into u8
-fn sextet_split(mut bits: BitVec<u8>) -> (Vec<u8>, usize) {
-	//because 8 is not a multiple of 6, some multiples of 8 (number of bits) are not multiples of 6, 
-	//so we add 0s at the end to make it divisible by both
-	//check how much padding is needed, and add it
-	let padding: usize = (bits.len() % 6).try_into().unwrap();
-	for _x in 0..padding {
-		bits.push(false);
-	}
-	//split, then shove into u8
-	let mut bits_slice: Vec<u8> = Vec::new();
-	//amount of sextets we have
-	let sextets: usize = (bits.len() / 6).try_into().unwrap();
-	//slice bits into sextets, change to u8 (char values) and shove char values into a vec
-	for x in 0..sextets {
-		let u = &bits[x..(x + 5)];
-		bits_slice.push(u.load::<u8>());
-	}
-	return (bits_slice, padding);
+pub fn encode(mut s: Vec<u8>) {
+    //stdin().read_to_end(&mut s).unwrap();
+    let a: BitVec<u8, Msb0> = BitVec::<u8, Msb0>::from_vec(s);
+    start(a);
 }
 
-type Language = String;
+/// Encode bits and add padding.
+fn start(bits: BitVec<u8, Msb0>) {
+    let charvalues = sextet_split(bits);
+    let encoded = value_to_chars(charvalues);
+    println!("{}", encoded);
+}
+
+/// Splits bitvec into 6-bit chunks then shoves them into `u8`.
+fn sextet_split(mut bits: BitVec<u8, Msb0>) -> Vec<u8> {
+    // Check how much input padding is needed, and add it.
+    let padding: usize = (6 - bits.len() % 6) % 6;
+    for _ in 0..padding {
+        bits.push(false);
+    }
+
+    // Split, then shove into `u8`.
+    let mut bits_slice: Vec<u8> = Vec::new();
+    // Amount of bits we have.
+    let nbits: usize = bits.len();
+    // Slice bits into sextets, then change to `u8` (char values) and shove char values into a vec.
+    for x in (0..nbits).step_by(6) {
+        let u: &BitSlice<u8, Msb0> = &bits[x..x + 6];
+        let v: u8 = u.load_be::<u8>();
+        bits_slice.push(v);
+    }
+    bits_slice
+}
+
+/// Takes a list of 6-bit values to a base-64 string.
 fn value_to_chars(charvalues: Vec<u8>) -> String {
-	let languages: Language = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/".to_owned();
-	let dictionary: Vec<char> = languages[..].chars().collect();
-	assert_eq!(dictionary.len(), 64);
-	let mut encode: Vec<char> = Vec::new();
-	for i in charvalues {
-		encode.push(dictionary[i as usize]);
-	}
-	return encode.iter().cloned().collect::<String>();
-}
+    // Check how much output padding is needed.
+    let padding: usize = (4 - charvalues.len() % 4) % 4;
 
-/*
-language: base64
-[Languages]
-base64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
-*/
+    let language: String =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/".to_owned();
+    let dictionary: Vec<char> = language.chars().collect();
+    assert_eq!(dictionary.len(), 64);
+    let mut encode: String = String::new();
+    for i in charvalues {
+        assert!(i < 0x40);
+        encode.push(dictionary[i as usize]);
+    }
+
+    // Add output padding.
+    for _ in 0..padding {
+        encode.push('=');
+    }
+
+    encode
+}
