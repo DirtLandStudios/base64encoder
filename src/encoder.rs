400b42b0d0738cf3bb50a1b4c49f3cf0d3160e3f
bananaboy319 <matteob319@gmail.com>
2022-04-11 15:24:21 +0200

    untested, but compiles

--- src/encoder.rs
+++ src/encoder.rs
@@ -1,12 +1,19 @@
 //use std::io::{stdin, Read};
 use bitvec::prelude::*;
 //base64: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
-pub fn encode(input: Vec<u8>, lang: String) {
+pub fn encode(input: Vec<u8>, lang: String) -> String {
     //stdin().read_to_end(&mut s).unwrap();
     let bits: BitVec<u8, Msb0> = BitVec::<u8, Msb0>::from_vec(input);
     let charvalues = sextet_split(bits);
-    let encoded = value_to_chars(charvalues, get_language(lang));
-    println!("{}", encoded);
+    let encoded: String = value_to_chars(charvalues, get_language(lang));
+    return encoded;
+}
+
+pub fn decode(input: String, lang: String) -> Vec<u8> {
+    let (values, padding) = chars_to_values(input.to_owned().chars().collect(), get_language(lang));
+    let bits: BitVec<u8, Msb0> = BitVec::<u8, Msb0>::from_vec(values);
+    let output = sextet_resplit(bits, padding);
+    return output;
 }
 
 /// Splits bitvec into 6-bit chunks then shoves them into `u8`.
@@ -27,20 +34,32 @@ fn sextet_split(mut bits: BitVec<u8, Msb0>) -> Vec<u8> {
         let v: u8 = u.load_be::<u8>();
         bits_slice.push(v);
     }
-    bits_slice
+    return bits_slice;
+}
+
+fn sextet_resplit(bits: BitVec<u8, Msb0>, padding: usize) -> Vec<u8> {
+    let mut bits_slice: BitVec<u8, Msb0> = BitVec::new();
+    // Amount of bits we have.
+    let nbits: usize = bits.len();
+    //get the important 6 of each byte, and shove into vec
+    for x in (0..nbits).step_by(8) {
+        let u: &BitSlice<u8, Msb0> = &bits[x..x + 6];
+        bits_slice.extend_from_bitslice(u);
+    }
+    // remove padding
+    bits_slice.drain((bits.len() - padding)..bits.len());
+    assert_eq!(bits_slice.len() % 8, 0);
+    
+    return bits_slice.into_vec();
 }
 
 /// Takes a list of 6-bit values to a base-64 string.
-fn value_to_chars(charvalues: Vec<u8>, lang: String) -> String {
+fn value_to_chars(charvalues: Vec<u8>, dictionary: Vec<char>) -> String {
     // Check how much output padding is needed.
     let padding: usize = (4 - charvalues.len() % 4) % 4;
-
-    let language: String = lang.to_owned();
-    let dictionary: Vec<char> = language.chars().collect();
-    assert_eq!(dictionary.len(), 64);
     let mut encode: String = String::new();
     for i in charvalues {
-        assert!(i < 0x40);
+        assert!(i < 0x40); //0x40 is 64 in hex
         encode.push(dictionary[i as usize]);
     }
 
@@ -49,17 +68,42 @@ fn value_to_chars(charvalues: Vec<u8>, lang: String) -> String {
         encode.push('=');
     }
 
-    encode
+    return encode;
 }
 
-fn get_language(file: String) -> String {
+fn get_language(file: String) -> Vec<char> {
+    let dictionary: Vec<char>;
     match std::fs::read_to_string(file) {
         Err(_) => {
-            let base64: String = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/".to_string();
-            return base64;
+            let base64: String = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
+                .to_string()
+                .to_owned();
+            dictionary = base64
+                            .chars()
+                            .collect();
         },
         Ok(lang) => {
-            return lang;
+            dictionary = lang
+                            .to_owned()
+                            .chars()
+                            .collect();
+            assert_eq!(dictionary.len(), 64);
         }
     }
+    return dictionary;
+}
+
+fn chars_to_values(input: Vec<char>, dictionary:Vec<char>) -> (Vec<u8>, usize) {
+    let mut padding = 0;
+    let mut ouput: Vec<u8> = Vec::new();
+    for x in input {
+        if x != '=' {
+            let pos: u8 = dictionary.iter().position(|&f| f == x).unwrap() as u8;
+            ouput.push(pos);
+        }
+        else {
+            padding += 2;
+        }
+    }
+    return (ouput, padding);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,18 @@
 mod encoder;
 use clap::Parser;
-
+use std::fs::{read, write};
+use std::str;
 
 #[derive(Parser)]
 #[clap(author, version, about, long_about = None)]
 struct Cli {
+	#[clap(short, long)]
+	decode: bool,
     #[clap(short, long)]
     path: Option<String>,
+	#[clap(short, long)]
 	string: Option<String>,
+	#[clap(short, long)]
 	language: Option<String>
 }
 
@@ -15,9 +20,9 @@ fn main() {
 	let cli = Cli::parse();
 	let mut input: Vec<u8> = Vec::new();
 	let mut lang_file: String = String::from("");
-
+	
 	if let Some(path) = cli.path.as_deref() {
-		input = std::fs::read(path).unwrap();
+		input = read(path).unwrap();
 	}
 	else if let Some(string) = cli.string.as_deref() {
 		input = string.as_bytes().to_vec();
@@ -25,7 +30,22 @@ fn main() {
 	if let Some(language) = cli.language.as_deref() {
 		lang_file = language.to_string();
 	}
-	encoder::encode(input, lang_file);
+	if cli.decode {
+		let output = encoder::decode(str::from_utf8(&input).unwrap().to_string(), lang_file);
+		match write("./out.txt", output) {
+			Ok(()) => {
+				println!("DONE")
+			},
+			Err(..) => {
+				println!("decode err")
+			}
+		}
+	}
+	else {
+		let encode = encoder::encode(input, lang_file);
+		println!("{}", encode);
+	}
+
 }
 
 
