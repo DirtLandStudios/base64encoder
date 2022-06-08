59da5666b7c1560fb2b68fcae68614d0ad92165e
bananaboy319 <matteob319@gmail.com>
2022-06-08 11:21:11 +0200

    make info

---
+++ make.sh
@@ -0,0 +1,4 @@
+#rustup toolchain install stable-x86_64-pc-windows-gnu
+#cargo build -r --target x86_64-pc-windows-gnu
+#rustup toolchain install stable-aarch64-apple-darwin
+#cargo build -r --target aarch64-apple-darwin
